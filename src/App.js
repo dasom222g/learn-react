abd8ab79d7d5f2f4fa31a9b76cdd582b520fcced
dasom222g <dasom228@gmail.com>
2023-06-21 14:40:17 +0900

    Add learn example

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Counter from './components/Counter';
+// import MyBox from './components/MyBox';
+// import YourBox from './components/YourBox';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      {/* 세상을 바꾸는건 꿈을꾸고 도전하는 사람들의 몫이다 */}
+      {/* <MyBox />
+      <MyBox />
+      <MyBox />
+      <MyBox />
+      <YourBox /> */}
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
